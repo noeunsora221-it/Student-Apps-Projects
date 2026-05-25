4ad3663cf56c6d952795dfa24e7931ee9a1f0672
Sophal Developer <noeunsora221@gmail.com>
2026-05-25 14:47:56 +0700

    Add files via upload

---
+++ app/modules/dashboard/views/dashboard_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
